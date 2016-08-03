8543a7a13a521333d4ce4207edf37e70ef1aa3e6
Justin Holzmann <jholzmann@8thlight.com>
2016-08-03 16:51:16 -0500

    WIP now that acceptance level test is up and running.

--- src/test/java/SocketWrapperTest.java
+++ src/test/java/SocketWrapperTest.java
@@ -1,13 +1,14 @@
 import HTTPServer.SocketWrapper;
 import junit.framework.TestCase;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 /**
  * Created by jphoenix on 8/2/16.
  */
 public class SocketWrapperTest extends TestCase {
-    public void testItHasAnInputStream() throws Exception {
-        SocketWrapper socket = new SocketWrapper();
+    public void testItImplementsGetRequest() throws Exception {
 
-        socket.connect(4444);
     }
 }
