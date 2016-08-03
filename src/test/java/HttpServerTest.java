8d2b0774041798b48db1183bb6ab305b7a703f4b
Justin Holzmann <jholzmann@8thlight.com>
2016-08-03 16:50:22 -0500

    Writes acceptance level test that will drive development.

--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -1,60 +1,30 @@
 import HTTPServer.Server;
 import junit.framework.TestCase;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 /**
  * Created by jphoenix on 7/28/16.
  */
 public class HttpServerTest extends TestCase {
     public void testItCanListenForAClient() throws Exception {
-        FakeServerSocket serverSocket = new FakeServerSocket();
+        MockServerSocket serverSocket = new MockServerSocket();
         Server server = new Server(serverSocket);
 
         server.run();
 
         assertEquals(true, serverSocket.listening);
-    }
-
-    public void testItCanHandleARequestFromAClient() throws Exception {
-        FakeSocket socket = new FakeSocket();
-        FakeServerSocket serverSocket = new FakeServerSocket(socket);
-        Server server = new Server(serverSocket);
-
-        server.run();
-
-        assertEquals(true, serverSocket.listening);
-    }
-
-    public void testItSendsBackHelloWorldToRequestToSlash() throws Exception {
-        String request = "GET / HTTP/1.1\nUser-Agent: Cake\nAccept-Language: en-us\n";
-        InputStream input = new ByteArrayInputStream(request.getBytes());
-        OutputStream output = new ByteArrayOutputStream();
-        FakeSocket socket = new FakeSocket(input, output);
-        FakeServerSocket serverSocket = new FakeServerSocket(socket);
-        Server server = new Server(serverSocket);
-
-        server.run();
-
     }
 
     public void testItSendsBackTheProperResponse() throws Exception {
         String request = "GET / HTTP/1.1\nUser-Agent: Cake\nAccept-Language: en-us\n";
-        String expectedResponse = "HTTP/1.1 200 OK\nUser-Agent: ServerCake\nContent-Type: text/html\n\n" +
+        String properResponse = "HTTP/1.1 200 OK\nUser-Agent: ServerCake\nContent-Type: text/html\n\n" +
                 "<html><body><h1>Hello world</h1></body></html>";
-        InputStream input = new ByteArrayInputStream(request.getBytes());
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        FakeSocket socket = new FakeSocket(input, output);
-        FakeServerSocket serverSocket = new FakeServerSocket(socket);
+        MockSocket socket = new MockSocket(request);
+        MockServerSocket serverSocket = new MockServerSocket(socket);
         Server server = new Server(serverSocket);
 
         server.run();
 
-        String response = output.toString();
-
-        assertEquals(expectedResponse, response);
+        String response = socket.getResponseValue();
+        assertEquals(properResponse, response);
     }
 }
