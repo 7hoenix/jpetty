512b56d4ea258d3bbe2ef181028bd9840fe02f84
Justin Holzmann <jholzmann@8thlight.com>
2016-08-04 18:00:23 -0500

    Rethinks interace to more general read, write and close.

--- src/main/java/HTTPServer/Connectible.java
+++ src/main/java/HTTPServer/Connectible.java
@@ -1,9 +1,12 @@
 package HTTPServer;
 
+import java.io.IOException;
+
 /**
  * Created by jphoenix on 8/1/16.
  */
 public interface Connectible {
-    String getRequest();
-    void sendResponse(String response);
+    String read() throws IOException;
+    void write(String response);
+    void close();
 }
