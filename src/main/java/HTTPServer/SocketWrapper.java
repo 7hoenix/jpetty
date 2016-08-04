746a38665c9c3e858abb35d3f2c28cbee834fe64
Justin Holzmann <jholzmann@8thlight.com>
2016-08-04 18:02:31 -0500

    Now takes an injected stream and converts it to a string.

--- src/main/java/HTTPServer/SocketWrapper.java
+++ src/main/java/HTTPServer/SocketWrapper.java
@@ -1,18 +1,39 @@
 package HTTPServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by jphoenix on 8/1/16.
  */
 public class SocketWrapper implements Connectible {
+    private InputStream input;
 
-    @Override
-    public String getRequest() {
-        return "GET /";
+    public SocketWrapper(InputStream inputStream) {
+       input = inputStream;
     }
 
     @Override
-    public void sendResponse(String response) {
+    public String read() throws IOException {
+        return convertStreamToString(input);
+    }
+
+    @Override
+    public void write(String response) {
+    }
+
+    public void close() {}
+
+    private String convertStreamToString(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        reader.close();
+        return out.toString();
     }
 }
