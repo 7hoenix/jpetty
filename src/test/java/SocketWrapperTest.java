551f6729d6b8c43245643f07419cf419e76f2b35
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 13:23:17 -0500

    Tests public methods of SocketWrapper.

--- src/test/java/SocketWrapperTest.java
+++ src/test/java/SocketWrapperTest.java
@@ -1,10 +1,8 @@
+import HTTPServer.Connectible;
 import HTTPServer.SocketWrapper;
-import com.oracle.tools.packager.IOUtils;
 import junit.framework.TestCase;
-import sun.nio.ch.IOUtil;
 
 import java.io.*;
-import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -12,12 +10,24 @@ import java.nio.charset.StandardCharsets;
  */
 public class SocketWrapperTest extends TestCase {
     public void testItTakesAnInputStreamAndReturnsAString() throws Exception {
-        String arbitaryString = "hey there cake 1234";
-        InputStream stream = new ByteArrayInputStream(arbitaryString.getBytes(StandardCharsets.UTF_8));
-        SocketWrapper socket = new SocketWrapper(stream);
+        String someStr = "hey there cake 1234";
+        InputStream streamIn = new ByteArrayInputStream(someStr.getBytes(StandardCharsets.UTF_8));
+        OutputStream streamOut = new ByteArrayOutputStream();
+        Connectible socket = new SocketWrapper(streamIn, streamOut);
 
         String request = socket.read();
 
         assertEquals("hey there cake 1234", request);
+    }
+
+    public void testWriteTakesAStringAndWritesToAnOutputStream() throws Exception {
+        String someStr = "hey there cake 1234";
+        InputStream streamIn = new ByteArrayInputStream(someStr.getBytes(StandardCharsets.UTF_8));
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Connectible socket = new SocketWrapper(streamIn, outputStream);
+
+        socket.write(someStr);
+
+        assertEquals("hey there cake 1234", outputStream.toString());
     }
 }
