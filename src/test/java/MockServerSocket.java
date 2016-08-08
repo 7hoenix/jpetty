ad16ebece7a9b4d423ca1517130cdc24ac44fc12
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 10:53:54 -0500

    I might need help with this.

--- src/test/java/MockServerSocket.java
+++ src/test/java/MockServerSocket.java
@@ -1,25 +1,30 @@
-import HTTPServer.ServerConnectable;
+import HTTPServer.ServerConnectible;
 import HTTPServer.Connectible;
 
 /**
  * Created by jphoenix on 8/1/16.
  */
-public class MockServerSocket implements ServerConnectable {
+public class MockServerSocket implements ServerConnectible {
 
     public boolean listening = false;
     private Connectible socket;
 
-    public MockServerSocket(MockSocket socket) {
+    public MockServerSocket(MockSocket socket)
+    {
         this.socket = socket;
     }
 
-    public MockServerSocket() {
-        socket = new MockSocket("GET /");
+    public MockServerSocket()
+    {
+        this.socket = new MockSocket("GET /");
     }
 
     @Override
-    public Connectible listenForRequest() {
+    public Connectible listen()
+    {
         listening = true;
         return socket;
     }
+
+    public void close() {}
 }
