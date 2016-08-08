50c37574f6f5d418c7540c9bf119f04db1d91c95
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 10:48:26 -0500

    Adds fileContents for helloWorld file and adds newlines after returns.

--- src/main/java/HTTPServer/HTTPRequestHandler.java
+++ src/main/java/HTTPServer/HTTPRequestHandler.java
@@ -6,20 +6,21 @@ import java.io.*;
  * Created by jphoenix on 8/4/16.
  */
 public class HTTPRequestHandler {
-    public String handle(String request) throws IOException {
+    public String handle(String request) throws IOException
+    {
         String[] sections = request.split(" ");
         if (sections[1].equals("/")) {
             String route = "./routes/helloWorld.html";
             InputStream input = new FileInputStream(new File(route));
             String fileContents = convertStreamToString(input);
-            return "HTTP/1.1 200 OK\rContent-Type: text/html\r\r" +
-                    "<html><body><h1>Hello world</h1></body></html>";
+            return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + fileContents;
         } else {
-            return "HTTP/1.1 404 NOT FOUND\r\r";
+            return "HTTP/1.1 404 NOT FOUND\r\n\r\n";
         }
     }
 
-    private String convertStreamToString(InputStream input) throws IOException {
+    private String convertStreamToString(InputStream input) throws IOException
+    {
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
         StringBuilder out = new StringBuilder();
         String line;
