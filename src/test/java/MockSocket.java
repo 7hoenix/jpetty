d90c3382cb742d7a3b35a98235abaa95902c8cef
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 10:54:42 -0500

    Mocks out input and output.

--- src/test/java/MockSocket.java
+++ src/test/java/MockSocket.java
@@ -7,25 +7,35 @@ import java.io.*;
  */
 public class MockSocket implements Connectible {
     private String input;
-    private String output = "";
+    private String output;
+    private OutputStream outputStream;
 
-    public MockSocket(String in) {
-        input = in;
+    public MockSocket(String input)
+    {
+        this.input = input;
     }
 
-    public String read() {
+    public MockSocket(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    public String read()
+    {
         return input;
     }
 
-    public void write(String response) {
-        output = response;
+    public void write(String response)
+    {
+        this.output = response;
     }
 
-    public void close() {
+    public void close()
+    {
 
     }
 
-    public String getResponseValue() {
+    public String displayValue()
+    {
         return output;
     }
 }
