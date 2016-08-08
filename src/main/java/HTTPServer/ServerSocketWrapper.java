586a1abb8ad06987f176da52c2f0d6e79c708714
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 10:51:10 -0500

    WIP Accept is not happening in listen.

--- src/main/java/HTTPServer/ServerSocketWrapper.java
+++ src/main/java/HTTPServer/ServerSocketWrapper.java
@@ -6,14 +6,21 @@ import java.net.ServerSocket;
 /**
  * Created by jphoenix on 8/1/16.
  */
-public class ServerSocketWrapper implements ServerConnectable {
-    private ServerSocket serverSocket;
+public class ServerSocketWrapper implements ServerConnectible {
+    private ServerConnectible serverSocket;
+    private Connectible socket;
+
+    public ServerSocketWrapper(Connectible socket) throws IOException {
+        this.socket = socket;
+    }
 
     public ServerSocketWrapper() throws IOException {
-        serverSocket = new ServerSocket(5000);
+        this.socket = new SocketWrapper(new ServerSocket(5000).accept());
     }
 
-    public Connectible listenForRequest() throws IOException {
-        return (Connectible) serverSocket.accept();
+    public Connectible listen() throws IOException {
+        return socket;
     }
+
+    public void close() {}
 }
