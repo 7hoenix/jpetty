f8c4bcdb4977d28da4555cf44956c9b974f3fbf8
Justin Holzmann <jholzmann@8thlight.com>
2016-08-08 10:47:32 -0500

    Connectible extends Closeable.

--- src/main/java/HTTPServer/Connectible.java
+++ src/main/java/HTTPServer/Connectible.java
@@ -1,12 +1,13 @@
 package HTTPServer;
 
+import java.io.Closeable;
 import java.io.IOException;
 
 /**
  * Created by jphoenix on 8/1/16.
  */
-public interface Connectible {
+public interface Connectible extends Closeable {
     String read() throws IOException;
-    void write(String response);
+    void write(String response) throws IOException;
     void close();
 }
