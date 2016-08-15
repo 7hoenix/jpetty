30cc1c7f68cf471a4c52a5577f526d8b9e81084d
Justin Holzmann <jholzmann@8thlight.com>
2016-08-15 13:50:31 -0500

    Now handles request in terms of bytes to match response type.

--- src/main/java/HTTPServer/HTTPRequestHandler.java
+++ src/main/java/HTTPServer/HTTPRequestHandler.java
@@ -1,6 +1,9 @@
 package HTTPServer;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,8 +20,10 @@ public class HTTPRequestHandler {
         this.rootDirectory = root;
     }
 
-    public String handle(String request) throws IOException {
-        Map parsedRequest = new RequestParser().parse(request);
+    public byte[] handle(InputStream request) throws IOException {
+        RequestParser parser = new RequestParser(request);
+        parser.parse();
+        Map parsedRequest = parser.getParams();
         String uri = "./".concat(rootDirectory).concat((String) parsedRequest.get("path"));
         File currentFile = new File(uri);
         if (parsedRequest.get("path").equals("/")) {
@@ -29,16 +34,15 @@ public class HTTPRequestHandler {
         } else if (currentFile.isFile()) {
             return writeFileContents(currentFile);
         } else {
-            return "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+            return "HTTP/1.1 404 NOT FOUND\r\n\r\n".getBytes();
         }
     }
 
-    private String generateDirectoryResponse(Map parsedRequest, File currentFile) {
+    private byte[] generateDirectoryResponse(Map parsedRequest, File currentFile) {
         String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html lang=\"en\"><body>";
         String[] filesInDir = currentFile.list();
         String parentDir = currentFile.getParentFile().getName();
         String links = "";
-
         if (!parentDir.equals(rootDirectory)) {
             String fullPath = currentFile.getParentFile().getPath();
             Integer rootLength = rootDirectory.length();
@@ -49,24 +53,43 @@ public class HTTPRequestHandler {
             links = links.concat("<a href=\"http://localhost:5000" + parsedRequest.get("path") + "/" + file + "\">" + file + "</a>\r\n");
         }
         response += links + "</body></html>";
-        return response;
+        return response.getBytes();
     }
 
-    private String writeFileContents(File currentFile) throws IOException {
-        InputStream input = new FileInputStream(currentFile);
-        String fileContents = convertStreamToString(input);
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + fileContents;
+    private byte[] writeFileContents(File currentFile) throws IOException {
+        byte[] fileInBytes = Files.readAllBytes(Paths.get(currentFile.getPath()));
+        byte[] response = addHeaders(currentFile, fileInBytes);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(response);
+        outputStream.write(fileInBytes);
+        return outputStream.toByteArray();
     }
 
-    private String convertStreamToString(InputStream input) throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        StringBuilder out = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            out.append(line);
+    private byte[] addHeaders(File currentFile, byte[] fileInBytes) {
+        String header = "HTTP/1.1 200 OK\r\n";
+        header = wrapContentType(header, currentFile, fileInBytes);
+        header += "\r\n\r\n";
+        return header.getBytes();
+    }
+
+    private String wrapContentLength(String header, byte[] fileInBytes) {
+        String size = new String(String.valueOf(fileInBytes.length));
+        return header + "Connection: close\r\nContent-Length: " + size;
+    }
+
+    private String wrapContentType(String header, File currentFile, byte[] fileInBytes) {
+        HashMap supportedTypes = new HashMap();
+        supportedTypes.put("jpg", "image/jpeg");
+        supportedTypes.put("gif", "image/gif");
+        supportedTypes.put("png", "image/png");
+        String path = currentFile.getPath();
+        int finalPeriod = path.lastIndexOf(".");
+        String extensionType = path.substring(finalPeriod + 1);
+        if (supportedTypes.get(extensionType) != null) {
+            header = header + "Content-Type: " + supportedTypes.get(extensionType) + "\r\n";
+            return wrapContentLength(header, fileInBytes);
+        } else {
+            return header + "Content-Type: text/html";
         }
-        reader.close();
-        return out.toString();
     }
 }
