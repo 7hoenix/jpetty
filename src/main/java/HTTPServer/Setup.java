7bd15e14cfc95ed136e6b4c5f77fce114d7d7d29
Justin Holzmann <jholzmann@8thlight.com>
2016-08-17 07:23:44 -0500

    Adds flags for setup with tests.

--- src/main/java/HTTPServer/Setup.java
+++ src/main/java/HTTPServer/Setup.java
@@ -1,19 +1,51 @@
 package HTTPServer;
 
-import java.io.File;
-
 /**
  * Created by jphoenix on 8/8/16.
  */
 public class Setup {
     public String rootDirectory;
+    public int port;
 
     public Setup(String[] args) {
-        if (pathGiven(args) && pathIsValid(args)) {
-            this.rootDirectory = stripEntry(args[0]);
+        assignPort(args);
+        assignRoot(args);
+    }
+
+    private void assignPort(String[] args) {
+        if (flagFound(args, "-p")) {
+            System.out.println("Port assigned");
+            this.port = Integer.parseInt(argAfterFlag(args, "-p"));
+        } else {
+            this.port = 5000;
+        }
+    }
+
+    private void assignRoot(String[] args) {
+        if (flagFound(args, "-d")) {
+            System.out.println("Root assigned");
+            this.rootDirectory = stripEntry(argAfterFlag(args, "-d"));
         } else {
             this.rootDirectory = "public";
         }
+    }
+
+    private String argAfterFlag(String[] args, String flag) {
+        int i = 0;
+        for (String arg : args) {
+            if (arg.equals(flag))
+                break;
+            i++;
+        }
+        return args[i + 1];
+    }
+
+    private boolean flagFound(String[] args, String flag) {
+        for (String arg : args) {
+            if (arg.equals(flag))
+                return true;
+        }
+        return false;
     }
 
     private String stripEntry(String arg) {
@@ -22,14 +54,5 @@ public class Setup {
         } else {
             return arg;
         }
-    }
-
-    private boolean pathIsValid(String[] args) {
-        File file = new File(args[0]);
-        return file.isDirectory();
-    }
-
-    private boolean pathGiven(String[] args) {
-        return args.length > 0;
     }
 }
--- src/test/java/SetupTest.java
+++ src/test/java/SetupTest.java
@@ -1,42 +1,43 @@
 import HTTPServer.Setup;
 import junit.framework.TestCase;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by jphoenix on 8/8/16.
  */
 public class SetupTest extends TestCase {
-    public void testItCanConfigureThePublicRoot() throws Exception {
-        String[] args = new String[1];
-        args[0] = "src";
-
-        Setup setup = new Setup(args);
-
-        assertEquals("src", setup.rootDirectory);
-    }
-
-    public void testItCanHandleASlashOrNoSlash() throws Exception {
-        String[] args = new String[1];
-        args[0] = "src/";
-
-        Setup setup = new Setup(args);
-
-        assertEquals("src", setup.rootDirectory);
-    }
-
-    public void testItDefaultsToPublic() throws Exception {
-        String[] args = new String[0];
+    public void testItHandlesThePFlagForPort() throws Exception {
+        String[] args = new String[2];
+        args[0] = "-p";
+        args[1] = "7500";
 
         Setup setup = new Setup(args);
 
         assertEquals("public", setup.rootDirectory);
+        assertEquals(7500, setup.port);
     }
 
-    public void testItDefaultsToPublicIfInvalid() throws Exception {
-        String[] args = new String[1];
-        args[0] = "cake";
+    public void testItTakesTheDFlagForDirectory() throws Exception {
+        String[] args = new String[2];
+        args[0] = "-d";
+        args[1] = "src";
 
         Setup setup = new Setup(args);
 
-        assertEquals("public", setup.rootDirectory);
+        assertEquals("src", setup.rootDirectory);
+        assertEquals(5000, setup.port);
+    }
+
+    public void testItTakesBothFlags() throws Exception {
+        String[] args = new String[4];
+        args[0] = "-d";
+        args[1] = "src";
+        args[2] = "-p";
+        args[3] = "7500";
+
+        Setup setup = new Setup(args);
+
+        assertEquals("src", setup.rootDirectory);
+        assertEquals(7500, setup.port);
     }
 }
