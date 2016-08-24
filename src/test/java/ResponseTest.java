611f8dd714f7d2ccef33cd53b0d5061f8118138b
Justin Holzmann <jholzmann@8thlight.com>
2016-08-24 10:51:58 -0500

    Adds basic getters and setters and full.

--- src/test/java/ResponseTest.java
+++ src/test/java/ResponseTest.java
@@ -1,49 +1,27 @@
 import HTTPServer.Response;
 import junit.framework.TestCase;
 
-import java.io.File;
-
 /**
  * Created by jphoenix on 8/24/16.
  */
 public class ResponseTest extends TestCase {
-    public void test_it_can_wrap_content_type() throws Exception {
-        File currentFile = new File("otherPublic/thing.txt");
-        byte[] initialResponse = new byte[0];
+    public void test_it_knows_its_header() throws Exception {
         Response response = new Response();
 
-        byte[] fullResponse = response.wrapContentType(initialResponse, currentFile);
+        response.setHeader("HTTP/1.1 200 OK\r\n".getBytes());
+        response.setBody("HI there.".getBytes());
 
-        assertEquals("Content-Type: text/plain\r\n", new String(fullResponse, "UTF-8"));
+        assertEquals("HTTP/1.1 200 OK\r\n", new String(response.getHeader(), "UTF-8"));
+        assertEquals("HI there.", new String(response.getBody(), "UTF-8"));
+        assertEquals("HTTP/1.1 200 OK\r\n\r\nHI there.", new String(response.getFull(), "UTF-8"));
     }
 
-    public void test_it_returns_the_response_if_no_type_is_found() throws Exception {
-        File currentFile = new File("otherPublic/fakeDirectory");
-        byte[] initialResponse = "HTTP/1.1 200 OK\r\n".getBytes();
+    public void test_it_returns_just_the_headers_if_no_body_is_found() throws Exception {
         Response response = new Response();
 
-        byte[] fullResponse = response.wrapContentType(initialResponse, currentFile);
+        response.setHeader("HTTP/1.1 200 OK\r\n".getBytes());
 
-        assertEquals("HTTP/1.1 200 OK\r\n", new String(fullResponse, "UTF-8"));
-    }
+        assertEquals("HTTP/1.1 200 OK\r\n\r\n", new String(response.getFull(), "UTF-8"));
 
-    public void test_it_can_wrap_content_length() throws Exception {
-        File currentFile = new File("otherPublic/thing.txt");
-        byte[] initialResponse = "HTTP/1.1 200 OK\r\n".getBytes();
-        Response response = new Response();
-
-        byte[] fullResponse = response.wrapContentLength(initialResponse, currentFile);
-
-        assertEquals("HTTP/1.1 200 OK\r\nContent-Length: 9\r\n", new String(fullResponse, "UTF-8"));
-    }
-
-    public void test_it_returns_the_response_if_no_length_is_found() throws Exception {
-        File currentFile = new File("otherPublic/fakeDirectory");
-        byte[] initialResponse = "HTTP/1.1 200 OK\r\n".getBytes();
-        Response response = new Response();
-
-        byte[] fullResponse = response.wrapContentLength(initialResponse, currentFile);
-
-        assertEquals("HTTP/1.1 200 OK\r\n", new String(fullResponse, "UTF-8"));
     }
 }
