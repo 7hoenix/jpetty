db278e9b85bcd4d963ecc2f329833d223d7b5b12
Justin Holzmann <jholzmann@8thlight.com>
2016-08-25 11:53:31 -0500

    ??? Cleans out settings section and makes generation of routes a different modules responsibility.

--- src/main/java/HTTPServer/Router.java
+++ src/main/java/HTTPServer/Router.java
@@ -5,36 +5,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Router {
-    private Setup settings;
     private final Map routes;
 
-    public Router(Setup settings) {
-        this.settings = settings;
-        this.routes = defaultRoutes();
-    }
-
     public Router(Map routes) {
-        this.settings = new Setup(new String[0]);
         this.routes = routes;
     }
 
     public Handler route(Map params) throws IOException {
-        return findRoute((String) params.get("action"));
-    }
-
-    private Handler findRoute(String action) {
-        if (routes.containsKey(action)) {
-            return (Handler) routes.get(action);
+        if (routes.containsKey(params.get("action"))) {
+            return (Handler) routes.get(params.get("action"));
         } else {
-            return new ErrorHandler(settings);
+            return (Handler) routes.get("ERROR");
         }
-    }
-
-    public Map defaultRoutes() {
-        HashMap<String, Handler> routes = new HashMap();
-        routes.put("GET", new GetHandler(settings));
-        routes.put("HEAD", new HeadHandler(settings));
-        routes.put("OPTIONS", new OptionsHandler(settings));
-        return routes;
     }
 }
