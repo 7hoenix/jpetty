813c18009874acb4ee0dbe5ec2dddf74e61a526c
Justin Holzmann <jholzmann@8thlight.com>
2016-08-25 13:04:47 -0500

    Changes test to state accurately what it is testing.

--- src/test/java/CobSpecApp/PostHandlerTest.java
+++ src/test/java/CobSpecApp/PostHandlerTest.java
@@ -7,9 +7,9 @@ import junit.framework.TestCase;
 import java.util.HashMap;
 
 public class PostHandlerTest extends TestCase {
-    public void test_it_posts_a_simple_html_file_to_the_file_structure() throws Exception {
+    public void test_it_passes_the_simple_post_spec() throws Exception {
         HashMap params = new HashMap();
-        params.put("action", "PUT");
+        params.put("action", "POST");
         params.put("path", "/games");
         PostHandler handler = new PostHandler(new Setup(new String[0]));
 
