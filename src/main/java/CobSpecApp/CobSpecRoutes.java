71fe7cb14a004d83edf2275c07cf85c63b44f899
Justin Holzmann <jholzmann@8thlight.com>
2016-08-25 11:59:21 -0500

    Moves route generation into CobSpecApp package under CobSpecRoutes.

---
+++ src/main/java/CobSpecApp/CobSpecRoutes.java
@@ -0,0 +1,17 @@
+package CobSpecApp;
+
+import HTTPServer.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CobSpecRoutes {
+    public static Map generate(Setup settings) {
+        HashMap<String, Handler> routes = new HashMap();
+        routes.put("GET", new GetHandler(settings));
+        routes.put("HEAD", new HeadHandler(settings));
+        routes.put("OPTIONS", new OptionsHandler(settings));
+        routes.put("POST", new PostHandler(settings));
+        return routes;
+    }
+}
---
+++ src/test/java/CobSpecRoutesTest.java
@@ -0,0 +1,22 @@
+import CobSpecApp.CobSpecRoutes;
+import HTTPServer.Handler;
+import HTTPServer.Response;
+import HTTPServer.Setup;
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CobSpecRoutesTest extends TestCase {
+    public void test_it_can_generate_a_map_of_routes_pointint_at_handlers() throws Exception {
+        Map routes = CobSpecRoutes.generate(new Setup(new String[0]));
+        Map params = new HashMap();
+        params.put("action", "GET");
+        params.put("path", "/");
+        Handler handler = (Handler) routes.get("GET");
+
+        Response response = handler.handle(params);
+
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n", new String(response.getHeader(), "UTF-8"));
+    }
+}
