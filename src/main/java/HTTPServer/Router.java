d08ffad96d8da63da9a386a1a99bb462fac9a363
Justin Holzmann <jholzmann@8thlight.com>
2016-08-30 14:33:11 -0500

    Uses new dynamic methods on request.

--- src/main/java/HTTPServer/Router.java
+++ src/main/java/HTTPServer/Router.java
@@ -13,8 +13,10 @@ public class Router {
     }
 
     public Handler route(Request request) throws IOException {
-        if (routes.containsKey(request.getParams().get("action"))) {
-            return routes.get(request.getParams().get("action"));
+        if (routes.containsKey(request.findQuery())) {
+            return routes.get(request.findQuery());
+        } else if (routes.containsKey(request.findAction())) {
+            return routes.get(request.findAction());
         } else {
             return new ErrorHandler("HTTP/1.1 405 NOT FOUND\r\n");
         }
