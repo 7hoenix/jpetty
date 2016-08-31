58f386f909ef03abe0812401959d25c8dcacb9bb
Justin Holzmann <jholzmann@8thlight.com>
2016-08-31 12:04:31 -0500

    Adds DataStorage interface and a DataStore class which implments it.

---
+++ src/test/java/Server/DataStoreTest.java
@@ -0,0 +1,33 @@
+package Server;
+
+import HTTPServer.DataStorage;
+import HTTPServer.DataStore;
+import junit.framework.TestCase;
+
+public class DataStoreTest extends TestCase {
+    public void test_it_stores_and_retrieves_an_entry() throws Exception {
+        DataStorage dataStore = new DataStore();
+        dataStore.store("cake", "yum");
+
+        assertEquals("yum", dataStore.retrieve("cake"));
+    }
+
+    public void test_it_can_update_an_entry() throws Exception {
+        DataStorage dataStore = new DataStore();
+        dataStore.store("pie", "yum");
+
+        dataStore.update("pie", "moar yum");
+
+        assertEquals("moar yum", dataStore.retrieve("pie"));
+
+    }
+
+    public void test_it_can_delete_an_entry_in_the_store() throws Exception {
+        DataStorage dataStore = new DataStore();
+        dataStore.store("pie", "more yum");
+
+        dataStore.remove("pie");
+
+        assertEquals("", dataStore.retrieve("pie"));
+    }
+}
