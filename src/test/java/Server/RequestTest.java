91a3dfe5f8b010468123c0f5071940a6315b2ac1
Justin Holzmann <jholzmann@8thlight.com>
2016-08-31 12:10:51 -0500

    Adds test for new findPostParams method on Request.

--- src/test/java/Server/RequestTest.java
+++ src/test/java/Server/RequestTest.java
@@ -69,6 +69,19 @@ public class RequestTest extends TestCase {
         assertEquals("Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?", params.get("variable_1"));
     }
 
+    public void test_it_parses_params_in_the_body_of_a_post_request() throws Exception {
+        byte[] fullRequest = ("POST /form HTTP/1.1\r\n\r\na=1&b=2").getBytes();
+        String header = "POST /form HTTP/1.1";
+        String body = "a=1&b=2";
+        Request request = new Request(fullRequest, header, body);
+
+        Map params = request.findPostParams();
+
+        assertEquals("1", params.get("a"));
+        assertEquals("2", params.get("b"));
+
+    }
+
     private Request getRequest(String query) {
         byte[] fullRequest = ("POST " + query + " HTTP/1.1\r\nContent-Length: 13\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nhi=mom").getBytes();
         String header = new String("POST " + query + " HTTP/1.1\r\nContent-Length: 13\r\nContent-Type: application/x-www-form-urlencoded\r\n");
