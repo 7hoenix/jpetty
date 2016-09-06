9573d5cb2e77d6c2ffccbff1877b597d2645a320
Justin Holzmann <jholzmann@8thlight.com>
2016-09-06 15:55:36 -0500

    New tests.

--- src/test/java/Server/ServerSocketWrapperTest.java
+++ src/test/java/Server/ServerSocketWrapperTest.java
@@ -1,20 +1,27 @@
 package Server;
 
-import HTTPServer.ServerSocketWrapper;
+import HTTPServer.Connection;
 import junit.framework.TestCase;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 
 public class ServerSocketWrapperTest extends TestCase {
-    public void testItReturnsAConnectible() throws Exception {
-        InputStream input = new ByteArrayInputStream("cake".getBytes());
-        MockSocket clientSocket = new MockSocket(input);
-        new Thread(clientSocket).start();
-        ServerSocketWrapper wrappedServerSocket = new ServerSocketWrapper(-1);
+    public void test_it_can_accept_a_new_connection() throws Exception {
+        Connection connection = new Connection(new MockSocket(new ByteArrayInputStream("".getBytes()), new ByteArrayOutputStream()));
+        MockServerSocket serverSocket = new MockServerSocket(connection, 1);
 
-        wrappedServerSocket.accept();
+        serverSocket.accept();
 
-        assertEquals(0, (int) wrappedServerSocket.connectionCount());
+        assertEquals(false, serverSocket.isListening());
+    }
+
+    public void test_it_can_get_closed() throws Exception {
+        Connection connection = new Connection(new MockSocket(new ByteArrayInputStream("".getBytes()), new ByteArrayOutputStream()));
+        MockServerSocket serverSocket = new MockServerSocket(connection, 1);
+
+        serverSocket.close();
+
+        assertEquals(false, serverSocket.isListening());
     }
 }
