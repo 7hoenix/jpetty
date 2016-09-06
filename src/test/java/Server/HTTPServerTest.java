5ea591710d44aa2857e03b71f4b0bc3c767359fc
Justin Holzmann <jholzmann@8thlight.com>
2016-09-06 15:53:17 -0500

    Makes test stay at level it probably should be.

--- src/test/java/Server/HTTPServerTest.java
+++ src/test/java/Server/HTTPServerTest.java
@@ -1,33 +1,25 @@
 package Server;
 
+import HTTPServer.Connection;
 import HTTPServer.Server;
 import junit.framework.TestCase;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class HTTPServerTest extends TestCase {
-    public void testItCanListenForAClient() throws Exception {
-        MockServerSocket serverSocket = new MockServerSocket(-1);
+    public void test_it_writes_to_the_output_stream() throws Exception {
+        InputStream input = new ByteArrayInputStream("GET / HTTP/1.1\r\n\r\n".getBytes());
+        OutputStream output = new ByteArrayOutputStream();
+        MockSocket socket = new MockSocket(input, output);
+        Connection connection = new Connection(socket);
+        MockServerSocket serverSocket = new MockServerSocket(connection, 1);
         Server server = new Server(serverSocket, new String[0]);
 
         server.run();
 
-        assertEquals(false, serverSocket.listening);
-    }
-
-    public void testItSendsBackTheProperResponse() throws Exception {
-        InputStream request = new ByteArrayInputStream("GET / HTTP/1.1\r\nUser-Agent: Cake\r\nAccept-Language: en-us\r\n\r\n".getBytes());
-        String properResponse = "HTTP/1.1 200 OK\r\nContent-Length: 71\r\nContent-Type: text/html\r\n\r\n" +
-                "<!DOCTYPE html><html lang=\"en\"><body><h1>Hello World</h1></body></html>";
-        MockSocket socket = new MockSocket(request);
-        MockServerSocket serverSocket = new MockServerSocket(socket, -1);
-        String[] args = new String[1];
-        args[0] = "-ai";
-        Server server = new Server(serverSocket, args);
-
-        server.run();
-
-        assertEquals(properResponse, socket.displayValue());
+        assertEquals(true, socket.isWritten());
     }
 }
